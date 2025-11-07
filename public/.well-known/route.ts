f9d882621bf393815623a54df8265b125e5daf17
KingZyphor <mediazyphor@gmail.com>
2025-11-07 08:48:05 +0000

    Updated Tab Title | Metadata

---
+++ public/.well-known/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return new NextResponse("discord-verification=dh=69d4b08f48690277d0fb919a4289d22178bd8dfd", {
+    headers: { "Content-Type": "text/plain" },
+  });
+}
